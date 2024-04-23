bdb50c050c791cee7c9306f05c2a1216a97fa6e8
Aleksandr <lipatov-fl@yandex.ru>
2024-04-24 00:00:17 +0300

    Добавил аутентификацию пользователя

--- app/src/main/java/com/example/todoapp/fragments/SignUpFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/SignUpFragment.kt
@@ -5,12 +5,63 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.todoapp.R
+import com.example.todoapp.databinding.FragmentSignInBinding
+import com.example.todoapp.databinding.FragmentSignUpBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+
 class SignUpFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var navController: NavController
+    private lateinit var binding: FragmentSignUpBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    ): View {
+        binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init(view)
+        registerEvents()
+    }
+
+    private fun init(view: View) {
+        navController = Navigation.findNavController(view)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    private fun registerEvents() {
+
+        binding.authTv.setOnClickListener{
+            navController.navigate(R.id.action_signUpFragment_to_signInFragment)
+        }
+
+        binding.btnFarther.setOnClickListener {
+            val email = binding.emailEt.text.toString().trim()
+            val pass = binding.passEt.text.toString().trim()
+            val rePass = binding.repassEt.text.toString().trim()
+
+            if (email.isNotEmpty() && pass.isNotEmpty() && rePass.isNotEmpty()) {
+                if (pass == rePass) {
+                    auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(
+                        OnCompleteListener {
+                            if (it.isSuccessful) {
+                                Toast.makeText(context, "Registered Successfully", Toast.LENGTH_SHORT).show()
+                                navController.navigate(R.id.action_signUpFragment_to_homeFragment)
+                            } else {
+                                Toast.makeText(context, it.exception?.message, Toast.LENGTH_SHORT).show()
+                            }
+                        })
+                }
+            }
+        }
     }
 }
