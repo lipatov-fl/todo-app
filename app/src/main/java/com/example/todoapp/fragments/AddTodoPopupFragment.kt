68d7450cc5e24cc8f0ba01cb02a24dd778efbf72
Aleksandr <lipatov-fl@yandex.ru>
2024-04-29 23:01:01 +0300

    Закончил работу добавления задач

--- app/src/main/java/com/example/todoapp/fragments/AddTodoPopupFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/AddTodoPopupFragment.kt
@@ -5,13 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
 import com.example.todoapp.R
 import com.example.todoapp.databinding.FragmentAddTodoPopupBinding
 import com.example.todoapp.databinding.FragmentHomeBinding
+import com.google.android.material.textfield.TextInputEditText
 
-class AddTodoPopupFragment : Fragment() {
+class AddTodoPopupFragment : DialogFragment() {
 
     private lateinit var binding: FragmentAddTodoPopupBinding
+    private lateinit var listener: DialogNextBtnClickListeners
+
+    fun setListener(listener: DialogNextBtnClickListeners) {
+        this.listener = listener
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,8 +35,20 @@ class AddTodoPopupFragment : Fragment() {
 
     private fun registerEvents() {
         binding.todoNextBtn.setOnClickListener {
-
+            val todoTask = binding.todoEt.text.toString()
+            if (todoTask.isNotEmpty()) {
+                listener.onSaveTask(todoTask, binding.todoEt)
+            } else {
+                Toast.makeText(context, "Please type some task", Toast.LENGTH_SHORT).show()
+            }
         }
+        binding.todoClose.setOnClickListener {
+            dismiss()
+        }
+    }
+
+    interface DialogNextBtnClickListeners {
+        fun onSaveTask(todo: String, todoEt: TextInputEditText)
     }
 
 }
--- app/src/main/java/com/example/todoapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/HomeFragment.kt
@@ -5,21 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.todoapp.R
 import com.example.todoapp.databinding.FragmentHomeBinding
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import java.lang.ref.Reference
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), AddTodoPopupFragment.DialogNextBtnClickListeners {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var databaseRef: DatabaseReference
     private lateinit var navController: NavController
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var popUpFragment: AddTodoPopupFragment
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,13 +39,28 @@ class HomeFragment : Fragment() {
 
     private fun registerEvents() {
         binding.addBtnHome.setOnClickListener {
-
+            popUpFragment = AddTodoPopupFragment()
+            popUpFragment.setListener(this)
+            popUpFragment.show(childFragmentManager, "AddTodoPopupFragment")
         }
     }
 
     private fun init(view: View) {
         navController = Navigation.findNavController(view)
         auth = FirebaseAuth.getInstance()
-        databaseRef = FirebaseDatabase.getInstance().reference
+        databaseRef = FirebaseDatabase.getInstance().reference.child("Tasks")
+            .child(auth.currentUser?.uid.toString())
+    }
+
+    override fun onSaveTask(todo: String, todoEt: TextInputEditText) {
+        databaseRef.push().setValue(todo).addOnCompleteListener {
+            if (it.isSuccessful) {
+                Toast.makeText(context, "Todo saved successfully", Toast.LENGTH_SHORT).show()
+                todoEt.text = null
+            } else {
+                Toast.makeText(context, it.exception?.message, Toast.LENGTH_SHORT).show()
+            }
+            popUpFragment.dismiss()
+        }
     }
 }
